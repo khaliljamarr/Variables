1b70a60f4437192f0487d410e99f0090baff0b2f
Khalil Jamarr <KhalilEdmondson@KhalilJamarr.local>
2020-12-03 10:17:57 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int test = 90;
+        float price = 9.99f;
+        String username = "khaliljamarr";
+
+        System.out.println("Test Score initialized is: " + test);
+        System.out.println("Price tag is: " + price);
+        System.out.println("Username: " + username);
+
+    }
+}
